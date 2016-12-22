1412e1ee1cc799f605a37c19864e375ad4a9ad9a
younglim <young@youngenius.com>
2016-12-22 10:29:03 +0800

    Update install-apps.sh

--- install-apps.sh
+++ install-apps.sh
@@ -9,8 +9,8 @@ wget http://www.syntevo.com/static/smart/download/smartgit/smartgit-8_0_1.deb
 sudo gdebi -n smartgit-8_0_1.deb
 
 # Install VirtualBox, Extension Pack and USB3.0 support
-wget http://download.virtualbox.org/virtualbox/5.1.10/virtualbox-5.1_5.1.10-112026~Ubuntu~yakkety_amd64.deb
-sudo gdebi virtualbox-5.1_5.1.10-112026~Ubuntu~yakkety_amd64.deb
+wget http://download.virtualbox.org/virtualbox/5.1.12/virtualbox-5.1_5.1.12-112440~Ubuntu~xenial_amd64.deb
+sudo gdebi virtualbox-5.1_5.1.12-112440~Ubuntu~xenial_amd64.deb
 wget http://download.virtualbox.org/virtualbox/5.1.10/Oracle_VM_VirtualBox_Extension_Pack-5.1.10-112026.vbox-extpack
 sudo VBoxManage extpack install --replace Oracle_VM_VirtualBox_Extension_Pack-5.1.10-112026.vbox-extpack
 sudo usermod -a -G vboxusers $USER
